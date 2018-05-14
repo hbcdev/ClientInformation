35ce14e22de6a607db6bd214ac6a4db86b3e2cc7
Noom Attpaon <noom.noon09@gmail.com>
2018-05-14 10:37:06 +0700

    commit tab interface

--- src/app/client-tab/client-tab.component.ts
+++ src/app/client-tab/client-tab.component.ts
@@ -9,15 +9,121 @@ export class ClientTabComponent implements OnInit {
   displayData = ['PolicyNo', 'Plan', 'Effective', 'Expired', 'PolicyYear'];
   displaySource = new MatTableDataSource<Element>(ELEMENT_DATA);
 
-  paymentTable = ['Period' , 'DueDate' , 'PiadDate' , 'Premium' , 'Filename'  ];
+  tabDataSow: any;
+
+  paymentTable = ['Period', 'DueDate', 'PiadDate', 'Premium', 'Filename'];
   paymentTableData = new MatTableDataSource<payment>(PAYMENT_DATA);
-  clientData = {};
+
   constructor() { }
 
   ngOnInit() {
+    this.tabDataSow =  TAB_DATA[0];
+    console.log('GG : ' + this.tabDataSow.tab1.Eclusion);
   }
 
 }
+
+export interface tabData {
+  tab1: string;
+  tab2: {
+    data: string;
+  };
+  tab3: {
+    Permium: string;
+    Waitting1: string;
+    Mobile: string;
+    Payee: string;
+    ClientNo: string;
+    Group: string;
+    Address: string;
+    Waitting2: string;
+    Note: string;
+    OldPolicy: string;
+    PolicyID: string;
+  };
+  tab4: {
+    OldCardNo: string;
+    AdjustDate: string;
+  };
+  tab6: {
+    Prefer: string;
+    Agent: string;
+    Agency: string;
+    AgentCode: string;
+    Address: string;
+    AgencyCode: string;
+  };
+  tab7: {
+    Quotation: string;
+    OldGracePeriod: string;
+    NewRecieve: string;
+    CancleRecieve: string;
+    LastUpdateUser: string;
+    DueDate: string;
+    NewUpload: string;
+    CancleUpload: string;
+    LastUpdateDate: string;
+  };
+  tab8: {
+    call: string;
+    date: string;
+  };
+}
+
+
+export const TAB_DATA: tabData[] = [
+  {
+    tab1: {
+      Eclusion: 'string',
+      InfoNote: 'string',
+      Notation: 'string'
+    },
+    tab2: {
+      data: 'string',
+    },
+    tab3: {
+      Permium: 'string',
+      Waitting1: 'string',
+      Mobile: 'string',
+      Payee: 'string',
+      ClientNo: 'string',
+      Group: 'string',
+      Address: 'string',
+      Waitting2: 'string',
+      Note: 'string',
+      OldPolicy: 'string',
+      PolicyID: 'string',
+    },
+    tab4: {
+      OldCardNo: 'string',
+      AdjustDate: 'string',
+    },
+    tab6: {
+      Prefer: 'string',
+      Agent: 'string',
+      Agency: 'string',
+      AgentCode: 'string',
+      Address: 'string',
+      AgencyCode: 'string',
+    },
+    tab7: {
+      Quotation: 'string',
+      OldGracePeriod: 'string',
+      NewRecieve: 'string',
+      CancleRecieve: 'string',
+      LastUpdateUser: 'string',
+      DueDate: 'string',
+      NewUpload: 'string',
+      CancleUpload: 'string',
+      LastUpdateDate: 'string',
+    },
+    tab8: {
+      call: 'string',
+      date: 'string',
+    },
+  }
+],
+
 export interface Element {
   PolicyNo: string;
   Plan: string;
